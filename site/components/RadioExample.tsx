0c28eec865d83693afc99d61f3fc8eb4d5959af1
Nitesh Phadatare <nitesh_phadatare@dell.com>
2020-10-11 21:08:28 +0530

    Addressing review comments

--- site/components/RadioExample.tsx
+++ site/components/RadioExample.tsx
@@ -5,15 +5,6 @@ import { useState } from 'preact/hooks';
 
 import { Radio } from '../../src/Radio';
 
-enum Answers {
-  Default = 'Default',
-  Option1 = 'HighText Machine Language',
-  Option2 = 'HyperText and links Markup Language',
-  Option3 = 'HyperText Markup Language'
-}
-
-type AnswerOption = keyof typeof Answers;
-
 const radioWrapperStyle = css`
   display: flex;
   margin: 1rem 0;
@@ -27,21 +18,18 @@ const radioStyle = css`
 
 // Argument props is required for 'path' attribute in AppRouter.tsx
 export function RadioExample(props: RoutableProps) {
-  const [ans, setAnswer] = useState<AnswerOption>(Answers.Default);
-  const selectedType = Answers[ans];
-
-  const onChange = (x: any) => setAnswer(x);
+  const [ans, setAnswer] = useState<string>('');
 
   return (
     <div>
       <h3>Component :- Radio</h3>
       <label>HTML stands for - </label>
       <div class={radioWrapperStyle}>
-        <Radio name="answer" class={radioStyle} value='Option1' onChange={onChange}>HighText Machine Language</Radio>
-        <Radio name="answer" class={radioStyle} value='Option2' onChange={onChange}>HyperText and links Markup Language</Radio>
-        <Radio name="answer" class={radioStyle} value='Option3' onChange={onChange}>HyperText Markup Language</Radio>
+        <Radio name="answer" class={radioStyle} value='HighText Machine Language' onChange={setAnswer as any}>HighText Machine Language</Radio>
+        <Radio name="answer" class={radioStyle} value='HyperText and links Markup Language' onChange={setAnswer as any}>HyperText and links Markup Language</Radio>
+        <Radio name="answer" class={radioStyle} value='HyperText Markup Language' onChange={setAnswer as any}>HyperText Markup Language</Radio>
       </div>
-      { selectedType ? <div>Selected value :- {selectedType}</div> : null }
+      { ans ? <div>Selected value :- {ans}</div> : null }
 
     </div>
   );
