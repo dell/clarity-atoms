1b8fc26a8b32b96659d359d4ca289ea9a5bd8aeb
Nitesh Phadatare <nitesh_phadatare@dell.com>
2020-10-11 16:06:23 +0530

    Addressing code review comments, removed RadioGroup

--- site/components/RadioExample.tsx
+++ site/components/RadioExample.tsx
@@ -3,13 +3,16 @@ import { h } from 'preact';
 import { RoutableProps } from 'preact-router';
 import { useState } from 'preact/hooks';
 
-import { Radio, RadioGroup } from '../../src/Radio';
+import { Radio } from '../../src/Radio';
 
 enum Answers {
-  'HighText Machine Language',
-  'HyperText and links Markup Language',
-  'HyperText Markup Language'
+  Default = 'Default',
+  Option1 = 'HighText Machine Language',
+  Option2 = 'HyperText and links Markup Language',
+  Option3 = 'HyperText Markup Language'
 }
+
+type AnswerOption = keyof typeof Answers;
 
 const radioWrapperStyle = css`
   display: flex;
@@ -24,18 +27,20 @@ const radioStyle = css`
 
 // Argument props is required for 'path' attribute in AppRouter.tsx
 export function RadioExample(props: RoutableProps) {
-  const [ans, setAnswer] = useState(-1);
-  const selectedType = Answers[ans]
+  const [ans, setAnswer] = useState<AnswerOption>(Answers.Default);
+  const selectedType = Answers[ans];
+
+  const onChange = (x: any) => setAnswer(x);
 
   return (
     <div>
       <h3>Component :- Radio</h3>
       <label>HTML stands for - </label>
-      <RadioGroup class={radioWrapperStyle} value={ans} onChange={setAnswer as any}>
-        <Radio class={radioStyle} value='0'>HighText Machine Language</Radio>
-        <Radio class={radioStyle} value='1'>HyperText and links Markup Language</Radio>
-        <Radio class={radioStyle} value='2'>HyperText Markup Language</Radio>
-      </RadioGroup>
+      <div class={radioWrapperStyle}>
+        <Radio name="answer" class={radioStyle} value='Option1' onChange={onChange}>HighText Machine Language</Radio>
+        <Radio name="answer" class={radioStyle} value='Option2' onChange={onChange}>HyperText and links Markup Language</Radio>
+        <Radio name="answer" class={radioStyle} value='Option3' onChange={onChange}>HyperText Markup Language</Radio>
+      </div>
       { selectedType ? <div>Selected value :- {selectedType}</div> : null }
 
     </div>
